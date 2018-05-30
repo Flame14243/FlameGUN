09a6fcda8e04e570e9101eef5e569f4c63bccacc
Flame14243 <37108357+Flame14243@users.noreply.github.com>
2018-05-30 11:25:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,15 @@
+const  Discord  =  gerektirir ( ' discord.js ' );
+const  client  =  yeni  Discord.Client ();
 
+istemci . on ( ' hazır ' , () => {
+    Konsol . log ( ' Hazırım! ' );
+});
+
+istemci . on ( ' message ' , mesaj  => {on
+    eğer ( mesaj . content  ===  ' ping ' ) {
+    	mesajı . cevap ( ' pong ' );
+  	}
+});
+
+// BU ŞEKİLDE OLMALIDIR
+istemci . giriş ( süreç . env . BOT_TOKEN );
